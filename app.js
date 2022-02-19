2efa250d452f86cc6e001dea71b1e691d2b8b2b3
ngranzow <nate.granzow@gmail.com>
2022-02-19 17:22:32 -0600

    modified the HTML template to handle more data

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js')
-
-// const pageHTML = generatePage(name, github);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -134,11 +132,11 @@ Add a New Project
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
 });
-
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//     if (err) throw new Error(err);
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+});
